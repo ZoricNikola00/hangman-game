b758ad1b60e08d01a9135462ce8c7b145b0e7023
ZoricNikola00 <zoriclfc@gmail.com>
2022-11-02 22:50:10 +0100

    Creating start function

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import { useState } from "react";
 import Hangman from "./components/Hangman";
-
+import {words} from './wordList'
 
 function App() {
   const [start,setStart]=useState(false);
   const [tries,setTries]=useState(0);
-  const [wordToGuess,setWordToGuess]=useState('')
+  const [wordToGuess,setWordToGuess]=useState<string[]>([])
   const [finalWord,setFinalWord]=useState('')
   const [count,setCount]=useState(0)
+  const [status,setStatus]=useState('')
+  const Start=()=>{
+    setTries(0)
+    setStart(false)
+    const newWord=words[Math.floor(Math.random()*words.length)]
+    setFinalWord(newWord)
+    const newArr=Array(newWord.length).join('.').split('.')
+    setWordToGuess(newArr)
+    setStatus('')
+  }
   return (
     <div className="text-center">
+        
         <Hangman tries={tries}/>  
     </div>
   );
