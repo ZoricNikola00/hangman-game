7f5db6aa13d10f0641f3d7509a848d7e1b88b133
ZoricNikola00 <zoriclfc@gmail.com>
2022-11-02 21:51:25 +0100

    Set home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 
 
 function App() {
+  const [start,setStart]=useState(false);
+  const [tries,setTries]=useState(0);
+  const [wordToGuess,setWordToGuess]=useState('')
+  const [finalWord,setFinalWord]=useState('')
+  const [count,setCount]=useState(0)
   return (
-    <>
-    </>
+    <div className="text-center">
+            
+    </div>
   );
 }
 
