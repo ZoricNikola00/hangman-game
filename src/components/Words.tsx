b5681559c4778d9c34c8595cd318236352b20982
ZoricNikola00 <zoriclfc@gmail.com>
2022-11-04 00:32:44 +0100

    Small edit

--- src/components/Words.tsx
+++ src/components/Words.tsx
@@ -4,9 +4,9 @@ type Data={
 }
 const Words = ({wordToGuess}:Data) => {
   return (
-    <div className='flex justify-center w-[300px] mx-auto'>
+    <div className='flex justify-center'>
         {wordToGuess && wordToGuess.map((word,i)=>{
-            return <div key={i} className={`text-black mx-1 w-[30px] sm:w-[60px] text-center text-4xl sm:text-6xl\ p-3 border-b-4 border-black`}><span className={`${word?'opacity-100':'opacity-0'}`}>{word?word.toUpperCase():'*'}</span></div>
+            return <div key={i} className={`text-black mx-1 flex justify-center items-center w-[30px] sm:w-[60px] text-center text-4xl sm:text-6xl p-3 border-b-4 border-black`}><span className={`${word?'opacity-100':'opacity-0'}`}>{word?word.toUpperCase():'*'}</span></div>
         })}
     </div>
   )
